ad4b1294e0b5bf61adbeb91b6e32e099a220615f
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2019-05-24 07:33:11 +0300

    add tests for clone

---
+++ repository_test.go
@@ -0,0 +1,28 @@
+package git
+
+import (
+	"testing"
+	"io/ioutil"
+	"os"
+)
+
+func TestClone(t *testing.T) {
+	dir, err := ioutil.TempDir("", "temp-clone-dir")
+	if err != nil {
+		t.Fatalf("Test Failed. error: %s", err.Error())
+	}
+	defer os.RemoveAll(dir) // clean up
+	opts := &CloneOptions{}
+	var tests = []struct {
+		inputDir string
+		inputURL string
+		inputOpt *CloneOptions
+	}{
+		{dir, "https://github.com/isacikgoz/.vim/", opts},
+	}
+	for _, test := range tests {
+		if _, err := Clone(test.inputDir, test.inputURL, test.inputOpt); err != nil {
+			t.Errorf("Test Failed. dir: %s, url: %s inputted, got \"%s\" as error.", test.inputDir, test.inputURL, err.Error())
+		}
+	}
+}
